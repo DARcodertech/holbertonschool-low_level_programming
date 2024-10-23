bf9cc3d378738f05d5ead4ee98f950f466a36b9c
root <root@DESKTOP-GV8C5Q5>
2024-10-23 14:04:38 +0200

    c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: 0
+ */
+int main(void) 
+{
+	int n;
+	char a;
+
+	for (n = 0 ; n < 10; n++)
+	{
+	putchar('0'+ n);
+	}
+	for (a = 'a' ; a <= 'f'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+return (0);
+}
