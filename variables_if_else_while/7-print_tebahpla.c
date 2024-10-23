2603b7bd8223c94975c66d07ad479c5ff4906721
root <root@DESKTOP-GV8C5Q5>
2024-10-23 13:22:28 +0200

    c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 'z'; z >= 'a' ; z--)
+	{
+	putchar(z);
+	}
+	putchar('\n');
+return (0);
+}
