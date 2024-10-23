83fcb830ac1926b1424d8b431fe94e639c530c0e
root <root@DESKTOP-GV8C5Q5>
2024-10-23 14:07:23 +0200

    c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -4,14 +4,14 @@
  *
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
 	int n;
 	char a;
 
 	for (n = 0 ; n < 10; n++)
 	{
-	putchar('0'+ n);
+	putchar('0' + n);
 	}
 	for (a = 'a' ; a <= 'f'; a++)
 	{
