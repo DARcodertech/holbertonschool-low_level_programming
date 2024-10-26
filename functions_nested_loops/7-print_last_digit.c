391f176caa1b77ef966cbcee2367f1ac31b967b4
root <root@DESKTOP-GV8C5Q5>
2024-10-26 21:37:10 +0200

    c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * print_last_digit - print a last digit number
  * @n: parameter of function
@@ -9,6 +8,7 @@
 int print_last_digit(int n)
 {
 	int print_last_digit = abs(n % 10);
+
 	_putchar(print_last_digit + '0');
 
 	return (print_last_digit);
