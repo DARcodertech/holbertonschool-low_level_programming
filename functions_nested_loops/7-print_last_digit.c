c1b1a6070a960481aa3493a7a491e7cd14e57940
root <root@DESKTOP-GV8C5Q5>
2024-10-26 21:18:19 +0200

    c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - print a last digit number
+ * @n: parameter of function
+ * Return: print last digit
+ */
+int print_last_digit(int n)
+{
+	int print_last_digit = abs(n) % 10;
+	_putchar(print_last_digit + '0');
+return (print_last_digit);
+}
