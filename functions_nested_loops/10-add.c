498fff7b67e3de923cc2daaa314888f56be44caa
root <root@DESKTOP-GV8C5Q5>
2024-10-27 14:17:17 +0100

    c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+ * add - fonction for calcul
+ * @a: number to add
+ * @b: number to add
+ * Return: return a result
+ */
 int add(int a, int b)
 {
 	int result;
+
 	result = a + b;
+
 	return (result);
 }
