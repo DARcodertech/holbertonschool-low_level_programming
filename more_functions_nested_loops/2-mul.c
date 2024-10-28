6eec466c7948cbd291f149ca0993511ea5fd85b8
root <root@DESKTOP-GV8C5Q5>
2024-10-28 10:45:37 +0100

    c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - fonction that calcu
+ * @a: parameter
+ * @b; parameter
+ * Return: return result of a, b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
