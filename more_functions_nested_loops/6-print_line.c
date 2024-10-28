0e2469372dbf3a83de59d78ac6f9b421cd5fea6a
root <root@DESKTOP-GV8C5Q5>
2024-10-28 16:10:07 +0100

    c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - print number of line
+ * @n: insert number of line
+ */
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
