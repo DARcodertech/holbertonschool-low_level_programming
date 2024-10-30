3de648b5e64e0efb68e0171679cc15bf0f5055d8
root <root@DESKTOP-GV8C5Q5>
2024-10-30 13:49:47 +0100

    c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int i; 
+	int j;
+	int t;
+
+	if (size > 0)
+	{
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 0; j < size - i; j++)
+	{
+	_putchar(' ');
+	}
+	for (t = 0; t < i; t++)
+	{
+	_putchar('#');
+	}
+	
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
