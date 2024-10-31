ba29df37e598ebffdf551e82a0ab7f29c00dae78
root <root@DESKTOP-GV8C5Q5>
2024-10-31 12:23:58 +0100

    c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - count length of a string
+ * @s: string to insert
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s)
+	{
+	length++;
+	s++;
+	}
+return (length);
+}
