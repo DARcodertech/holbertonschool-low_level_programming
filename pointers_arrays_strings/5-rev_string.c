93b16a56784ac54491905589db1e9bce94d8c7f5
root <root@DESKTOP-GV8C5Q5>
2024-11-01 22:32:26 +0100

    c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+/**
+ * rev_string - reverse a string
+ * @s: parameter for string
+ */
+void rev_string(char *s)
+{
+	int length;
+	int l;
+	char temp; 
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	
+	}
+	for (l = 0; l < length /2; l++)
+	{
+	temp = s[l];
+	s[l] = s[length - l - 1];
+	s[length - l - 1] = temp;
+	}
+
+}
