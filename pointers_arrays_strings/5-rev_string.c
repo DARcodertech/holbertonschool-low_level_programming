6b3e0bbaafacfc52c9c75e4e5a24ae52310456ed
root <root@DESKTOP-GV8C5Q5>
2024-11-01 22:34:36 +0100

    c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,13 @@ void rev_string(char *s)
 {
 	int length;
 	int l;
-	char temp; 
+	char temp;
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
-	
+
 	}
-	for (l = 0; l < length /2; l++)
+	for (l = 0; l < length / 2; l++)
 	{
 	temp = s[l];
 	s[l] = s[length - l - 1];
