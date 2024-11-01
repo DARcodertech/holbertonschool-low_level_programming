294209f57e19c16abfdd17bc64a23568165a0c9b
root <root@DESKTOP-GV8C5Q5>
2024-11-01 22:01:06 +0100

    c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}	
