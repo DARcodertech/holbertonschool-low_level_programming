eeed6b21816c64b412b8935e44b28868bc65e2f2
root <root@DESKTOP-GV8C5Q5>
2024-11-02 21:26:01 +0100

    c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - copies a string
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i; 
+	for (i = 0; i < n; i++)
+	{
+	if (*src != '\0')
+	{
+	dest[i] = *src;
+	src++;
+	}
+	else
+	{
+	dest[i] = '\0';
+	}
+	}
+return (dest);
+}
