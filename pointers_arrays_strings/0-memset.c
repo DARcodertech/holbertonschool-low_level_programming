7f9b160c8dc9921e3d1416ab43846a8b9f66bcd7
root <root@DESKTOP-GV8C5Q5>
2024-11-04 15:47:52 +0100

    c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fills a memory with a constant byte
+ * @s: string or array
+ * @b: char
+ * @n: number
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+return (s);
+}
--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,6 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int res = 0;
-	int sig = 1;
-
-	if (s[0] == '-')
-	{
-	sig = -1;
-	i++;
-	}
-	for(; s[i] != '\n'; i++)
-	res = res*10 + s[i] - '0';
-	
-
-return (sig*res);
+	int p = 0;
+	int sign = 0;
 }
