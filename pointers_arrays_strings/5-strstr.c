775a88763bcf88b23b68176066401aab924afe0c
root <root@DESKTOP-GV8C5Q5>
2024-11-06 12:18:51 +0100

    c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+/**
+ * _strstr -  locates a substring
+ * @haystack: parameter
+ * @neddle: parameter
+ * Return: NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == '\0')
+	{
+	return (haystack);
+	}
+	
+	while (*haystack != '\0')
+	{
+	if (*haystack == *needle)
+	{
+	
+	while (needle[i] != '\0' && haystack[i] == needle[i])
+	{
+	i++;
+	}
+	if (needle[i] == '\0')
+	{
+	return haystack;
+	}
+	}
+	haystack++;
+	}
+return (NULL);
+}	
