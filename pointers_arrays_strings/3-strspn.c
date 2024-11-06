b6fbc1e87a039e97f404744520f06f21812edb9d
root <root@DESKTOP-GV8C5Q5>
2024-11-06 10:45:53 +0100

    c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: parameter
+ * @accapet: parameter
+ * Return: count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	unsigned int i;
+	unsigned int j;
+	int n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	n = 0;
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	{
+	n = 1;
+	}
+	}
+	if (n == 0)
+	{
+	return (count);
+	}
+	count++;
+	}
+return (count);
+}
