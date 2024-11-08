c79c50b40f2367fd6789b0c3447243cf81ba8812
root <root@DESKTOP-GV8C5Q5>
2024-11-08 15:15:58 +0100

    c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+/**
+ * _sqrt_recursion - returns the natural square
+ * @n: number
+ * Return:
+ */
+int _sqrt_again(int n, int guess);
+
+int _sqrt_recursion(int n)
+{
+	return _sqrt_again (n, 1);
+}
+/** _sqrt_again - returns the natural square
+ * @n: number
+ * Return: function
+ */
+int _sqrt_again(int n, int l)
+{
+	if (l * l == n)
+	{
+	return (l);
+	}
+	if (l * l > n)
+	{
+	return (-1);
+	}
+	return (_sqrt_again(n, l + 1));
+}
