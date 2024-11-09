08ca2f84087305456969ad7b9406edf1eb215b6d
root <root@DESKTOP-GV8C5Q5>
2024-11-09 11:40:51 +0100

    c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
