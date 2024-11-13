6b6a739f81ca5147e2faeec28c248e104f2b16b2
root <root@DESKTOP-GV8C5Q5>
2024-11-13 11:28:31 +0100

    c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer
+ * @width: parameter
+ * @height: parameter
+ * Return: g
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g;
+	int i;
+        int j;
+
+	if (width <= 0 || height <= 0)
+	{
+	return (NULL);
+	}
+	g = malloc(sizeof(int *) * height);
+	if (g == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	g[i] = malloc(sizeof(int) * width);
+	if (g[i] == NULL)
+	{
+	for (j = 0; j < i; j++)
+	{
+	free(g);
+	return (NULL);
+	}
+	for (j = 0; j < width; j++)
+	{
+	g[i][j] = 0;
+	}
+	}
+	}
+	return (g);
+}
