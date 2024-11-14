22189f8a2fc818d9119161ec0388fd180603ab90
root <root@DESKTOP-GV8C5Q5>
2024-11-14 15:33:39 +0100

    c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,8 +1,15 @@
 #include <stddef.h>
 #include "dog.h"
+/**
+ * init_dog - initialize a variable
+ * @d: structureof dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d == NULL)
+	if (d == NULL)
 	return;
 
 	d->name = name;
