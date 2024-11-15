d2210ea0baf64bad413ac9ba7a3b0dbc6b3d322a
root <root@DESKTOP-GV8C5Q5>
2024-11-15 17:30:51 +0100

    c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,27 +11,43 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int l_o;
+	int l_n;
+	int i = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 	return (NULL);
 	}
-	new_dog->name = malloc(strlen(name) + 1);
+	while (name[l_n] != '\0') {
+	l_n++;
+	}
+	new_dog->name = malloc(l_n + 1);
 	if (new_dog->name == NULL)
 	{
 	free(new_dog);
 	return (NULL);
 	}
-	strcpy(new_dog->name, name);
-	new_dog->owner = malloc(strlen(owner) + 1);
+	while (i <= l_n) {
+	new_dog->name[i] = name[i];
+	i++;
+	}
+	while (owner[l_o] != '\0') {
+	l_o++;
+	}
+	new_dog->owner = malloc(l_o + 1);
 	if (new_dog->owner == NULL)
 	{
 	free(new_dog->name);
 	free(new_dog);
 	return (NULL);
 	}
-	strcpy(new_dog->owner, owner);
+	i = 0;
+	while (i <= l_o) {
+	new_dog->owner[i] = owner[i];
+	i++;
+	}
 	new_dog->age = age;
 	return (new_dog);
-}
+	}
