2198a385ed34616bb68b247f19ac475efd1282fb
root <root@DESKTOP-GV8C5Q5>
2024-11-20 10:06:57 +0100

    c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,3 +1,21 @@
-void array_iterator(int *array, size_t size, void (*action)(int));
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - execute o, each element of an array
+ * @array: array
+ * @size: size
+ * @action: action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+	return;
+	}
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
+}
