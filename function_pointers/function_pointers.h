c4986573342b8ba2d97e5b64222c22b1f7d5a878
root <root@DESKTOP-GV8C5Q5>
2024-11-22 16:23:41 +0100

    c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTER.H
-#define FUNCTION_POINTER.H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 #include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
