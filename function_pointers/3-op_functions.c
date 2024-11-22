a3176476e38cd03bd1fac7441c6f03aa6e933faf
root <root@DESKTOP-GV8C5Q5>
2024-11-22 14:58:51 +0100

    c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function
+ * @a: parameter
+ * @b: parameter
+ * Return: plus
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function
+ * @a: parameter
+ * @b: parameter
+ * Return: minus
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function
+ * @a: parameter
+ * @b: parameter
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function
+ * @a: parameter
+ * @b: parameter
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function
+ * @a: parameter
+ * @b: parameter
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
