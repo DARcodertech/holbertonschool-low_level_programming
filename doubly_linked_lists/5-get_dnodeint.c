ea8f6fc5c8774977fb6158c3a376076b6f3faf92
root <root@DESKTOP-GV8C5Q5>
2024-12-06 11:27:09 +0100

    c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function
+ * @head: parameter
+ * @index: parameter
+ * Return: NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int count = 0;
+
+while (head != NULL)
+{
+if (count == index)
+{
+return (head);
+}
+head = head->next;
+count++;
+}
+return (NULL);
+}
