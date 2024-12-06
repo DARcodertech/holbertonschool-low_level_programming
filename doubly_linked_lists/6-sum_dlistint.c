8b81e9fcf1cb572b336f1ac9c6b3e59e12e8c008
root <root@DESKTOP-GV8C5Q5>
2024-12-06 11:35:44 +0100

    c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function
+ * @head: parameter
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
