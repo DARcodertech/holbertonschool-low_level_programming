298198e22d8d966bd8c35bdca48cfa2156132749
root <root@DESKTOP-GV8C5Q5>
2024-12-08 13:44:19 +0100

    c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * create_file - function
+ * @filename: parameter
+ * @text_context: parameter
+ * Return: -1 or 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t bytes_written;
+size_t length = 0;
+
+if (filename == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+while (text_content[length])
+{
+length++;
+}
+bytes_written = write(fd, text_content, length);
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+if (close(fd) == -1)
+{
+return (-1);
+}
+return (1);
+}
